d494b59a55099d79010f05f32a0a187391b85f5f
mjalzate11 <mjalzate11@gmail.com>
2021-11-12 17:36:39 -0500

    Commit Maicol Correa Alzate - 3801911646

---
+++ app/src/main/java/com/df/persistencia/Carros.java
@@ -0,0 +1,14 @@
+package com.df.persistencia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Carros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carros);
+    }
+}
