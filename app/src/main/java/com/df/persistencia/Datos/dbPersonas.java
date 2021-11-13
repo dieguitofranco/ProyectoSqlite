761ff2de14b27db5b7b5934159e472683f9e7199
Diego Franco <dieguitofranco5>
2021-11-12 22:00:20 -0500

    se crea funcion para eliminar adiconar y mostrar  personas

--- app/src/main/java/com/df/persistencia/Datos/dbPersonas.java
+++ app/src/main/java/com/df/persistencia/Datos/dbPersonas.java
@@ -4,9 +4,12 @@ import android.app.Person;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.widget.Toast;
 
 import com.df.persistencia.Model.Persona;
+
+import java.util.ArrayList;
 
 public class dbPersonas {
 
@@ -19,7 +22,16 @@ public class dbPersonas {
     public void insertPerson(Persona p){
         ContentValues cv = new ContentValues();
         cv.put(Estructuras.ColumnasPersona.name,p.getName());
+        cv.put(Estructuras.ColumnasPersona.documento,p.getDocumento());
+        cv.put(Estructuras.ColumnasPersona.edad, p.getEdad());
+        cv.put(Estructuras.ColumnasPersona.email, p.getEmail());
+        cv.put(Estructuras.ColumnasPersona.telefono, p.getTelefono());
         db.insert(BaseDatosCarros.Tablas.Personas,null, cv);
+    }
+
+    public void deletePersona(String document){
+        db.delete(BaseDatosCarros.Tablas.Personas,Estructuras.ColumnasPersona.documento + "=?",new String[] {document});
+        db.close();
     }
 
     public int conteoPersonas(){
@@ -33,6 +45,27 @@ public class dbPersonas {
         }
         c.close();
         return  conteo;
+    }
+
+
+
+    public ArrayList<Persona> obtenerPersonas(){
+        ArrayList<Persona> personas = new ArrayList<>( );
+        Cursor c = db.rawQuery("Select * from personas", null);
+        if (c.getCount() > 0 ){
+            c.moveToFirst();
+            do{
+                Persona per = new Persona();
+                per.setDocumento(c.getString(c.getColumnIndex(Estructuras.ColumnasPersona.documento)));
+                per.setEdad(c.getInt(c.getColumnIndex(Estructuras.ColumnasPersona.edad)));
+                per.setEmail(c.getString(c.getColumnIndex(Estructuras.ColumnasPersona.email)));
+                per.setName(c.getString(c.getColumnIndex(Estructuras.ColumnasPersona.name)));
+                per.setTelefono(c.getString(c.getColumnIndex(Estructuras.ColumnasPersona.telefono)));
+                personas.add(per);
+            }while (c.moveToNext());
+        }
+        c.close();
+        return  personas;
     }
 
 
---
+++ app/src/main/java/com/df/persistencia/Views/Activities/Adapters/PersonaAdaptador.java
@@ -0,0 +1,67 @@
+package com.df.persistencia.Views.Activities.Adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import com.df.persistencia.Model.Persona;
+import com.df.persistencia.R;
+import com.squareup.picasso.Picasso;
+
+public class PersonaAdaptador extends BaseAdapter {
+    
+    protected Activity activity;
+    protected ArrayList<Persona> personas;
+
+    public PersonaAdaptador(Activity activity, ArrayList<Persona> personas) {
+        this.activity = activity;
+        this.personas = personas;
+    }
+
+    @Override
+    public int getCount() {
+        return personas.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return personas.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        View v = view;
+        if (view == null){
+            LayoutInflater inf = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inf.inflate(R.layout.person_item, null);
+        }
+        Persona person = personas.get(i);
+        TextView textNamePerson = v.findViewById(R.id.textNamePerson);
+        TextView textDocument = v.findViewById(R.id.textDocument);
+        TextView textEdad = v.findViewById(R.id.textEdad);
+        TextView textEmail = v.findViewById(R.id.textEmail);
+        TextView textPhone = v.findViewById(R.id.textPhone);
+        ImageView img = v.findViewById(R.id.imgPerson);
+        textNamePerson.setText(person.getName());
+        textDocument.setText(person.getDocumento());
+        textEdad.setText(String.valueOf(person.getEdad()));
+        textEmail.setText(person.getEmail());
+        textPhone.setText(person.getTelefono());
+        Picasso.get().load("https://thumbs.dreamstime.com/z/icono-de-la-muestra-del-usuario-s%C3%ADmbolo-de-la-persona-avatar-humano-84519083.jpg").into(img);
+
+        return v;
+    }
+}
