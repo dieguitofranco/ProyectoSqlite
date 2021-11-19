912fd3203328423446030a40652fbf9f5d1e5a47
mjalzate11 <mjalzate11@gmail.com>
2021-11-19 18:31:59 -0500

    .

--- app/src/main/java/com/df/persistencia/Views/Activities/Activities/Carros.java
+++ app/src/main/java/com/df/persistencia/Views/Activities/Activities/Carros.java
@@ -2,15 +2,59 @@ package com.df.persistencia.Views.Activities.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.df.persistencia.Datos.dbCarros;
+import com.df.persistencia.Model.Carro;
 import com.df.persistencia.R;
+import com.df.persistencia.Views.Activities.Adapters.CarroAdaptador;
 
-public class Carros extends AppCompatActivity {
+
+import java.util.ArrayList;
+
+public class Carros extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
+
+    private Button btnCrear;
+    private ListView listaCarro;
+    dbCarros dbc;
+    ArrayList<Carro> carros;
+    CarroAdaptador carAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carros);
+        btnCrear = findViewById(R.id.btnFormCarros);
+        listaCarro = findViewById(R.id.lstCarros);
+        listaCarro.setOnItemClickListener(this);
+        listaCarro.setOnItemLongClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.btnCrear:
+                intent = new Intent(this, FomularioCarro.class);
+                startActivity(intent);
+                break;
+
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this, "Ha seleccionado el carro: ", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        return false;
     }
 }
